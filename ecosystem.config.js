e24caf83314c8a91a17bb508467b9c6f44707c86
JeongCheolLee <jclee7503@gmail.com>
2022-08-23 00:15:50 +0900

    fix: ecosysyem confg typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,11 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: '172.31.38.79',
-      path: '/home/ubuntu/memoir_prd.pem',
+      host: '15.164.103.217',
+      path: '/home/ubuntu/memoir_server',
       repo: 'git@github.com:hocheolworks/memoir_server.git',
       ref: 'origin/main',
-      key: '/Users/jeongcheol/memoir_server.pem',
+      key: '/Users/jeongcheol/memoir_prd.pem',
       'post-deploy': 'npm i; pm2 reload ecosystem.config.js --env production',
     },
   },
