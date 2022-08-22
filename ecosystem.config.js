c038bdfa4173cb3951617a7f95b4b9e4c92e6dd1
JeongCheolLee <jclee7503@gmail.com>
2022-08-23 00:35:47 +0900

    Test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,7 @@ module.exports = {
       repo: 'git@github.com:hocheolworks/memoir_server.git',
       ref: 'origin/main',
       key: '/Users/jeongcheol/memoir_prd.pem',
+      'pre-deploy': 'npm i; npm run build',
       'post-deploy':
         'npm i; npm run build; pm2 start dist/main.js --name memoir_server;',
     },
