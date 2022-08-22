891c97ef1344780e1203de96d8355261efb25c81
JeongCheolLee <jclee7503@gmail.com>
2022-08-23 00:05:16 +0900

    feat: ci/cd config added

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'memoir_server',
+      script: 'index.js',
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: '172-31-38-79',
+      path: '/home/ubuntu/memoir_server',
+      repo: 'git@github.com:hocheolworks/memoir_server.git',
+      ref: 'origin/main',
+      key: '/Users/jeongcheol/memoir_server.pem',
+      'post-deploy': 'npm i; pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
