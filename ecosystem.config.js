f4bb268ff65f36b01f8ea533b465d6eacf35a540
JeongCheolLee <jclee7503@gmail.com>
2022-08-27 16:13:15 +0900

    feat: git pull 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,7 @@ module.exports = {
       repo: 'git@github.com:hocheolworks/memoir_server.git',
       ref: 'origin/main',
       key: '/Users/jeongcheol/memoir_prd.pem',
+      'pre-build': 'git pull',
       'post-deploy':
         'npm i; npm run build; pm2 start dist/main.js --name memoir_server --watch;',
     },
