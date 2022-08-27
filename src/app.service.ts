034e62aabb28d057f04c79ff8277f53770d31c00
JeongCheolLee <jclee7503@gmail.com>
2022-08-27 16:04:19 +0900

    feat : envs test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '이 글을 본다면 ci/cd에 성공한것 우하하';
+    return 'env upload ci/cd test';
   }
 }
