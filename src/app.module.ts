256b3c35116726ad34df08771a085946dd0ad63f
JeongCheolLee <jclee7503@gmail.com>
2022-08-28 16:26:57 +0900

    feat : package.json script 시작 재시작 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ if (process.env.NODE_ENV === 'production') {
   console.log(envFilePath);
   console.log(process.env.DB_HOST);
   console.log(process.env.DB_USERNAME);
+  console.log(process.env);
 }
 @Module({
   imports: [
