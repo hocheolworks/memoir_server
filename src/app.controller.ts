be0fe5b7b7cd351845fe34ea453e036bf464e0c7
JeongCheolLee <jclee7503@gmail.com>
2022-08-31 23:59:17 +0900

    test controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('temp')
+  getHello2(): string {
+    return this.appService.getHello();
+  }
 }
