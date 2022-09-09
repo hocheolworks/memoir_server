3e2aa367d1168f4ca5cd6c8ad7f70961ec7bc8e5
JeongCheolLee <jclee7503@gmail.com>
2022-09-09 14:50:49 +0900

    feat: auth module import

--- src/user/dtos/github-sign-up.dto.ts
+++ src/user/dtos/github-sign-up.dto.ts
@@ -1,5 +1,5 @@
 import { PickType } from '@nestjs/swagger';
-import UserInfo from '../user.entity';
+import UserInfo from '../../user/user.entity';
 
 export class GithubSignUpDto extends PickType(UserInfo, [
   'githubId',
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GithubCodeDto } from './dtos/user.dto';
+import { AuthService } from '../auth/auth.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import UserService from './user.service';
 
 @ApiTags('users')
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private authService: AuthService,
+  ) {}
 
   @ApiOperation({
     summary: '깃허브 로그인 및 회원가입',
     description: '깃허브 로그인',
   })
+  @UseGuards(JwtAuthGuard)
   @Post('/login')
   async githubLogin(@Body() githubCodeDto: GithubCodeDto) {
-    const user = await this.userService.githubLogin(githubCodeDto);
+    const user = await this.authService.login(githubCodeDto);
 
     return user;
   }
