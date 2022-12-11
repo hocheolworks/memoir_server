40aeb952be3d7f18103bcf87d4e9c9657b65776e
JeongCheolLee <jclee7503@gmail.com>
2022-12-11 15:47:19 +0900

    레포지토리 초기화

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'env upload ci/cd test2';
-  }
-}
--- src/git/git.constants.ts
+++
@@ -1,8 +0,0 @@
-const gitConstants = {
-  requestUrl: {
-    createRepository: 'https://api.github.com/user/repos',
-  },
-  errorMessages: {},
-};
-
-export default gitConstants;
