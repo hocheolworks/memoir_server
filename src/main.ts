84dd109b331ed3a637af5e621ac7c1555b977d3e
Bliss96 <jclee7503@gmail.com>
2023-03-10 17:58:42 +0900

    ing : swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { WinstonLogger } from './common/helpers/winstonLogger';
 
@@ -6,6 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonLogger,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Memoir_APIS')
+    .setDescription('월드클래스 메모아의 API 문서')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
