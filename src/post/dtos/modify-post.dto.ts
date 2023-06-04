e7110e80b64befe5f21cc6c7e8011fc5c667c25a
Bliss96 <jclee7503@gmail.com>
2023-06-04 19:28:48 +0900

    feat: 게시글 수정 DTO에서 조회수 안보이게 수정

--- src/post/dtos/modify-post.dto.ts
+++ src/post/dtos/modify-post.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty } from 'class-validator';
 import { User } from 'src/user/user.entity';
 import { PostDto } from './post.dto';
 
-export class ModifyPostDto extends PickType(PostDto, ['postTitle', 'views']) {
+export class ModifyPostDto extends PickType(PostDto, ['postTitle']) {
   @ApiProperty({
     example:
       '현재 진행하고 있는 사이드 프로젝트에서 next와 redux를 함께 사용하는 과정 중, next-redux-wrapper에 대해 큰 오해를 하고있었고, 나와 같은 실수를 하는 사람이 없길 바라며 몇자 적어본다.',
@@ -31,4 +31,5 @@ export class ModifyPostDto extends PickType(PostDto, ['postTitle', 'views']) {
   accessToken?: string;
   user?: User;
   postUrl?: string;
+  views: number;
 }
