04024026ee5217ea5c8a28e98990e2fea9577ec2
JeonCheolLee <jclee7503@gmail.com>
2023-11-24 10:39:45 +0900

    feat: 코드 정리

--- src/post/services/post.service.ts
+++ src/post/services/post.service.ts
@@ -174,11 +174,8 @@ export class PostService {
       throw new NotFoundException(constants.errorMessages.POST_NOT_FOUND);
     }
 
-    const token = process.env.GIT_TOKEN;
-
     const headers = {
       Accept: 'application/vnd.github+json',
-      Authorization: token,
     };
 
     let getPostRequest: AxiosResponse;
